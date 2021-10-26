ab5cc8ff3a6f4e427bcfd4d4cf686bf0cad856f4
Ivanova Anna <annprokudina@gmail.com>
2021-10-26 12:17:23 +0300

    Initial Commit

---
+++ hw_3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw_3.1
+//
+//  Created by Anna Ivanova on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
